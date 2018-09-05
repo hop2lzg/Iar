43c81ea56c5db00c5fb43f0fede99a8beb0fc2ef
hop2lzg <sli1@majestic-vacations.com>
2018-09-05 15:20:53 +0800

    WebSocket

--- arc_download_assign_DL.py
+++ arc_download_assign_DL.py
@@ -1,8 +1,12 @@
-import time, datetime
+import time
+import datetime
 import ConfigParser
 import arc
-# import thread
 import threading
+import hashlib
+import socket
+import base64
+import os
 
 
 class MyThread(threading.Thread):
@@ -22,6 +26,80 @@ class MyThread(threading.Thread):
             logger.fatal(ex)
         finally:
             thread_lock.release()
+
+
+class WebSocketThread(threading.Thread):
+    def __init__(self, connection):
+        super(WebSocketThread, self).__init__()
+        self.connection = connection
+
+    def __send(self, data):
+        self.connection.send('%c%c%s' % (0x81, len(data), data))
+
+    def run(self):
+        print 'new websocket client joined!'
+        data = self.connection.recv(1024)
+        re = parse_data(data)
+        print re
+        self.__send("LOADING")
+        thread_set(is_this_week, csv_lines, is_first_arc_number)
+        self.__send("OVER")
+
+        if csv_lines:
+            self.__send("CREATE FILE")
+            body = "\r\n".join(csv_lines)
+            file_path = download_file_path
+            file_name = "all_SA_CA_DL.csv"
+            arc_model.csv_write(file_path, file_name, body)
+            self.__send("download file:" + file_name)
+        else:
+            self.__send("SORRY FILED!")
+
+
+class LoopThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        max_count = 60
+        for i in range(max_count):
+            print i, is_socket_accepted
+            if (i >= max_count-1) and not is_socket_accepted:
+                os._exit(0)
+            elif is_socket_accepted:
+                break
+            else:
+                time.sleep(1)
+
+
+def parse_data(msg):
+    v = ord(msg[1]) & 0x7f
+    if v == 0x7e:
+        p = 4
+    elif v == 0x7f:
+        p = 10
+    else:
+        p = 2
+    mask = msg[p:p + 4]
+    data = msg[p + 4:]
+
+    return ''.join([chr(ord(v) ^ ord(mask[k % 4])) for k, v in enumerate(data)])
+
+
+def parse_headers(msg):
+    headers = {}
+    header, data = msg.split('\r\n\r\n', 1)
+    for line in header.split('\r\n')[1:]:
+        key, value = line.split(': ', 1)
+        headers[key] = value
+    headers['data'] = data
+    return headers
+
+
+def generate_token(msg):
+    key = msg + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+    ser_key = hashlib.sha1(key).digest()
+    return base64.b64encode(ser_key)
 
 
 def execute(name, is_this_week, csv_lines, is_first_arc_number):
@@ -64,13 +142,16 @@ def execute(name, is_this_week, csv_lines, is_first_arc_number):
                 continue
 
             # print "create list"
-            create_list_html = arc_model.create_list(token, ped, action, arc_number)
+            create_list_html = arc_model.create_list(token, ped, action, arc_number, selectedStatusId="",
+                                                     selectedTransactionType="SA", selectedFormOfPayment="CA",
+                                                     dateTypeRadioButtons="ped", viewFromDate=from_date, viewToDate=to_date,
+                                                     selectedNumberOfResults="20")
             token = arc_regex.create_list(create_list_html)
 
             if not token:
                 continue
             csv_text = arc_model.get_csv(name, is_this_week, ped, action, arc_number, token, from_date, to_date,
-                              transaction_type="SA", form_of_payment="CA", tick=tick)
+                                         selectedTransactionType="SA", selectedFormOfPayment="CA", tick=tick)
 
             if csv_text:
                 lines = csv_text.split('\r\n')
@@ -130,28 +211,64 @@ tick = date_time.strftime('%Y%m%d%H%M%S')
 csv_lines = []
 is_first_arc_number = True
 
+HOST = conf.get("socket", "host")
+PORT = int(conf.get("socket", "port"))
+
+logger.debug("HOST: %s, PORT: %d" % (HOST, PORT))
+
+download_file_path = conf.get("download", "filePath")
+
 is_this_week = False
 if date_time.weekday() >= 1:
     is_this_week = True
 
-thread_set(is_this_week, csv_lines, is_first_arc_number)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((HOST, PORT))
+sock.listen(5)
+is_socket_accepted = False
+# print "loop thread"
+loop_thread = LoopThread()
+loop_thread.start()
+
+logger.debug("accept start")
+connection, address = sock.accept()
+is_socket_accepted = True
+logger.debug("accept end")
+try:
+    data = connection.recv(1024)
+    headers = parse_headers(data)
+    token = generate_token(headers['Sec-WebSocket-Key'])
+    connection.send('\
+HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n\
+Upgrade: WebSocket\r\n\
+Connection: Upgrade\r\n\
+Sec-WebSocket-Accept: %s\r\n\r\n' % token)
+    thread = WebSocketThread(connection)
+    thread.start()
+except socket.timeout:
+    logger.warn("WebSocket connection timeout")
+    # print 'WebSocket connection timeout'
+
+
+# thread_set(is_this_week, csv_lines, is_first_arc_number)
 # logger.debug("ALL lines: %s" % csv_lines)
 # ##---------------------------send email
 # print "start send email"
-if csv_lines:
-    mail_smtp_server = conf.get("email", "smtp_server")
-    mail_from_addr = conf.get("email", "from")
-    mail_to_addr = conf.get("email", "to_arc_download_assign").split(';')
-    mail_subject = 'IAR DOWNLOAD SA CA'
-    try:
-        body = "\r\n".join(csv_lines)
-        floder = "csv"
-        file_name = "all_SA_CA.csv"
-        arc_model.csv_write(floder, file_name, body)
-        mail = arc.Email(smtp_server=mail_smtp_server)
-        mail.send(mail_from_addr, mail_to_addr, mail_subject, "Please check the attachment.", [floder + "/" + file_name])
-        logger.debug("sent email")
-    except Exception as e:
-        logger.critical(e)
+# if csv_lines:
+#     mail_smtp_server = conf.get("email", "smtp_server")
+#     mail_from_addr = conf.get("email", "from")
+#     mail_to_addr = conf.get("email", "to_arc_download_assign").split(';')
+#     mail_subject = 'IAR DOWNLOAD SA CA'
+#     try:
+#         body = "\r\n".join(csv_lines)
+#         floder = "csv"
+#         file_name = "all_SA_CA.csv"
+#         arc_model.csv_write(floder, file_name, body)
+#         mail = arc.Email(smtp_server=mail_smtp_server)
+#         mail.send(mail_from_addr, mail_to_addr, mail_subject, "Please check the attachment.", [floder + "/" + file_name])
+#         logger.debug("sent email")
+#     except Exception as e:
+#         logger.critical(e)
 
 logger.debug("<<<<<<<<<<<<<<<<<<<<<END>>>>>>>>>>>>>>>>>>>>>")
